1c488a8209836652a049e203c7b5ab2a4628f85d
Chad Galloway <chad45@gmail.com>
2021-05-27 22:59:03 -0500

    create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Chad Galloway on 5/27/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
