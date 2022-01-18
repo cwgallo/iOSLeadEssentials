7636ad92174b6fcac2300905cef72382d59d32b5
Chad Galloway <chad45@gmail.com>
2022-01-18 00:54:01 -0600

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Chad Galloway on 1/18/22.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
